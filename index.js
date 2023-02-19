17e4e3caff3748f48db2e711c421e9cb9e379a56
Yogesh SA <yogesh.5prog@gmail.com>
2023-02-19 19:56:56 +0530

    Done

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-let button = document.querySelector('.button')
-let search = document.querySelector('.search-bar')
+let weather = {
+  apikey : "ec3eaff725a8986b385ad4640faba8f5",
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+this.apikey)
+   .then((response) => response.json())
+   .then((data) => this.displayWeather(data));
+ },
+ displayWeather: function(data){
+    var { name }  = data;
+    const { description} = data.weather[0];
+    const { temp , humidity} = data.main;
+    const { speed } = data.wind;
+    console.log(name, description, temp, humidity, speed);
+    document.querySelector('.city').innerHTML = "Weather in " + name;
+    document.querySelector('.temp').innerHTML = "Temperature " +temp+" °C";
+    document.querySelector('.description').innerHTML = description;
+    document.querySelector('.humidity').innerHTML = "Humidity: " +humidity+"g.m-3";
+    document.querySelector('.wind').innerHTML = "Wind: "+speed+" km/hr";
+ },
+search: function() {
+  this.fetchWeather(document.querySelector(".search-bar").value)
+}
+};
 
-button.addEventListener("click", function fetchWeather(city){
-  fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=ec3eaff725a8986b385ad4640faba8f5")
-  .then((Response) => {
-      if(!Response.ok){
-         alert("No weather found.");
-          throw new Error("No weather found.");
-      }
-      return Response.json();
-  })
-  .then((data) => this.displayWeather(data));
-})
+document.querySelector(".search button").addEventListener("click", function(){
+  weather.search();
+});
 
-   
-  
-
-  function displayWeather(data){
-     const { name } = data;
-     const { description } = data.weather;
-     const{temp,humidity} = data.main;
-     const {speed} = data.wind;
-     document.querySelector(".city").innerText = "weather in" + name;
-     document.querySelector(".description").innerText = description;
-     document.querySelector(".temp").innerText = temp + "degree C";
-     document.querySelector(".humidity").innerText = humidity;
-     document.querySelector(".wind").innerText = speed + "km/hr";
-  }
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-         if (event.key == "Enter") {
-           search();
-         }
-        
-})
+document.querySelector('.search-bar').addEventListener("keypress", function(){
+  weather.search();
+});
 
  
 
