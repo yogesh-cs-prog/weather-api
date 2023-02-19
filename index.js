811258ad10a20e64d3e0f35362992b9ee6b46dd1
Yogesh SA <yogesh.5prog@gmail.com>
2023-02-19 17:36:12 +0530

    failed

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 let button = document.querySelector('.button')
-  function fetchWeather(city){
-      fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=ec3eaff725a8986b385ad4640faba8f5")
-      .then((Response) =>{
-          if(!Response.ok){
-             alert("No weather found.");
-              throw new Error("No weather found.");
-          }
-          return Response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  }
+let search = document.querySelector('.search-bar')
+
+button.addEventListener("click", function fetchWeather(city){
+  fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=ec3eaff725a8986b385ad4640faba8f5")
+  .then((Response) => {
+      if(!Response.ok){
+         alert("No weather found.");
+          throw new Error("No weather found.");
+      }
+      return Response.json();
+  })
+  .then((data) => this.displayWeather(data));
+})
+
+   
+  
+
   function displayWeather(data){
      const { name } = data;
      const { description } = data.weather;
@@ -21,17 +27,12 @@ let button = document.querySelector('.button')
      document.querySelector(".humidity").innerText = humidity;
      document.querySelector(".wind").innerText = speed + "km/hr";
   }
-  
-  function search(){
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  }
-  document.querySelector('.button').addEventListener("click", search())
-  
-  .querySelector(".search-bar").addEventListener("keyup", function (event) {
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
          if (event.key == "Enter") {
-           weather.search();
+           search();
          }
-        })
+        
+})
 
  
 
